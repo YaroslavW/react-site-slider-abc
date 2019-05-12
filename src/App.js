7445f6542cf54793f548dfd041400c4dc0b68cb2
Yaroslav <doctor355@yahoo.com>
2019-05-12 16:42:20 +0300

    Creating the main structure and connecting the header

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './resources/style.css';
+import Header from './Compomemts/Header_Footer/Header';
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+    </div>
+  );
+}
+
+export default App;
