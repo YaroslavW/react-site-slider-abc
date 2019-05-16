e7e4e766b6bf6bc127c1f04807a382997ac3165a
Yaroslav <doctor355@yahoo.com>
2019-05-16 11:14:21 +0300

    Description component

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import './resources/style.css';
 import Header from './Compomemts/Header_Footer/Header';
 import Slider from './Compomemts/Slider';
 import Info from './Compomemts/Info';
+import Highlights from './Compomemts/Highlights';
 function App() {
   return (
     <div className="App">
       <Header />
       <Slider />
       <Info />
+      <Highlights />
     </div>
   );
 }
