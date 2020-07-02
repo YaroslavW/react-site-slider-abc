818ba1b6e1c8f6dbceb91d1d87eb93fa184647b3
Yaroslav <doctor355@yahoo.com>
2020-07-02 15:38:23 +0300

    added dinamic deadline date

--- src/Compomemts/Slider/Timer.js
+++ src/Compomemts/Slider/Timer.js
@@ -2,8 +2,64 @@ import React, {useState, useEffect} from "react";
 import Slide from "react-reveal/Slide";
 
 const Timer = () => {
-  const  deadline = 'Dec, 16, 2019';
+  let deadline = dni();
+  function dni() {
+    var D = new Date();
+    D.setDate(D.getDate() + 50);
+    
+    var month = D.getMonth();
+    
+    function LitMonth(month) {
+      switch (month) {
+        case 0:
+          month = "Jan";
+          break;
+        case 1:
+          month = "Feb";
+          break;
+        case 2:
+          month = "Mar";
+          break;
+        case 3:
+          month = "Apr";
+          break;
+        case 4:
+          month = "May";
+          break;
+        case 5:
+          month = "Jun";
+          break;
+        case 6:
+          month = "Jul";
+          break;
+        case 7:
+          month = "Aug";
+          break;
+        case 8:
+          month = "Sep";
+          break;
+        case 9:
+          month = "Oct";
+          break;
+        case 10:
+          month = "Nov";
+          break;
+        case 11:
+          month = "Dec";
+          break;
 
+        default:
+          
+      }
+      return month;
+    }
+    month = LitMonth(month);
+    var day = D.getDate();
+    var year = D.getFullYear();
+    D = month + " " + day + " " + year;
+    return D;
+  }
+   
   const [days, setDays] = useState('0');
   const [hours , setHours] = useState('0');
   const [minutes, setMinutes] = useState('0');
